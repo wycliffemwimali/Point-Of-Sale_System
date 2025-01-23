c3689c87b69f487f91854415d685ced858fb1982
Wycliffe Mwimali <wycliffemwimali2@gmail.com>
2025-01-23 17:34:49 +0300

    pos system

---
+++ src/main/java/com/wycliffe/pointofsalesystem/PointOfSaleSystemApplication.java
@@ -0,0 +1,13 @@
+package com.wycliffe.pointofsalesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PointOfSaleSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PointOfSaleSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wycliffe/pointofsalesystem/PointOfSaleSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wycliffe.pointofsalesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PointOfSaleSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
